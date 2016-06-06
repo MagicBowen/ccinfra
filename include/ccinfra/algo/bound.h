80d7ae9d5a4941ddf1cb24fe57776e3b69b88881
wang.bo <wang.bo123@zte.com.cn>
2016-06-06 19:24:06 +0800

    reduce the duplication of bound.h

--- include/ccinfra/algo/bound.h
+++ include/ccinfra/algo/bound.h
@@ -3,78 +3,56 @@
 
 #include "ccinfra/base/BaseTypes.h"
 
-template<typename T>
-U32 ccinfra_lower_bound(const  T array[], U32 len, T key)
+namespace details
 {
-    U32 low  = 0;
-    U32 high = len - 1;
-    U32 mid  = 0;
-
-    if(key < array[low])
+    template <typename T, bool isLowBound>
+    size_t ccinfra_bound(const T* array, size_t len, T key)
     {
-        return low;
-    }
+        size_t low  = 0;
+        size_t high = len - 1;
+        size_t mid  = 0;
 
-    if(key > array[high])
-    {
-        return high;
-    }
-
-    while(low <= high)
-    {
-        mid = (low + high) >> 1;
-        if(key == array[mid])
+        if(key < array[low])
         {
-            return mid;
+            return low;
         }
 
-        if(key > array[mid])
+        if(key > array[high])
         {
-            low  = mid + 1;
+            return high;
         }
-        else
+
+        while(low <= high)
         {
-            high = mid - 1;
+            mid = (low + high) >> 1;
+            if(key == array[mid])
+            {
+                return mid;
+            }
+
+            if(key > array[mid])
+            {
+                low  = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
         }
+        return isLowBound? high : low;
     }
-    return high;
 }
 
 template<typename T>
-U32 ccinfra_upper_bound(const  T array[], U32 len, T key)
+size_t ccinfra_lower_bound(const T* array, size_t len, T key)
 {
-    U32 low  = 0;
-    U32 high = len - 1;
-    U32 mid  = 0;
+    return details::ccinfra_bound<T, true>(array, len, key);
+}
 
-    if(key < array[low])
-    {
-        return low;
-    }
-
-    if(key > array[high])
-    {
-        return high;
-    }
-
-    while(low <= high)
-    {
-        mid = (low + high) >> 1;
-        if(key == array[mid])
-        {
-            return mid;
-        }
-
-        if(key > array[mid])
-        {
-            low  = mid + 1;
-        }
-        else
-        {
-            high = mid - 1;
-        }
-    }
-    return low;
+template<typename T>
+size_t ccinfra_upper_bound(const T* array, size_t len, T key)
+{
+    return details::ccinfra_bound<T, false>(array, len, key);
 }
 
 #endif
